c2c0bd688428185d685c61ce30b53d57270a1db1
theUnspecified <222mzb@gmail.com>
2017-04-27 03:27:03 +0600

    Nasa earth data processing

---
+++ test3.py
@@ -0,0 +1,27 @@
+from pandas import read_csv
+from pandas.tools.plotting import  scatter_matrix
+from matplotlib import pyplot
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import classification_report
+from sklearn.metrics import  confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import utils
+from sklearn import preprocessing
+import numpy as np
+#read data
+filename = 'temp.csv'
+dataset = read_csv(filename)
+
+array = dataset.values
+x = array[:, 0:2]
+y = array[:2]
+
+print(y)
